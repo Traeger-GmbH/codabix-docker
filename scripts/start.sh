b83db1d286d6d8b180c53b02f6a912c592e78943
f.traeger <f.traeger@traeger.de>
2019-11-26 11:20:01 +0100

    add possibility to restore an existing codabix configuration backup

--- scripts/start.sh
+++ scripts/start.sh
@@ -14,13 +14,18 @@ if [[ (-z "${CODABIX_INITIALIZED}" ) && (! -f "${CODABIX_PROJECT_DIR}/codabixdb.
     echo "admin pw: ${CODABIX_ADMIN_PASSWORD}"
 
     codabix init --projectDirectory:${CODABIX_PROJECT_DIR}
+
+    # restore project configuration if environment variable for restore is set
+    if [ -n "${CODABIX_RESTORE_FILE}" ]; then
+        codabix restore ${CODABIX_RESTORE_FILE} --projectDirectory:${CODABIX_PROJECT_DIR}
+    fi
+
     if [ -n "${CODABIX_PROJECT_NAME}" ]; then
         codabix settings "{ \"ProjectName\": \"${CODABIX_PROJECT_NAME}\" }" --projectDirectory:${CODABIX_PROJECT_DIR}
     fi
     codabix setAdminPassword ${CODABIX_ADMIN_PASSWORD} --projectDirectory:${CODABIX_PROJECT_DIR}
 
     unset ${CODABIX_ADMIN_PASSWORD}
-    
     export CODABIX_INITIALIZED=true
 fi
 
